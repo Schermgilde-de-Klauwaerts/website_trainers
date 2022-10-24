96c086b4e1584b73d93841b0a684c84318e7d672
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-10-24 17:17:51 +0200

    laura stinkt niet meer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Laura je stinkt een beetje
+          Laura je ruikt heel lekker vandaag
         </p>
         <a
           className="App-link"
