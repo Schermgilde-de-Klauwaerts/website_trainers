e23cc538f64bc1948be0cf7136295c5ad48251f9
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-10-28 23:51:38 +0200

    mock aantal dagen per maand

---
+++ src/api/mock_aantal_dagen_per_maand.js
@@ -0,0 +1,16 @@
+const AANTALDAGENPERMAAND = [
+  31, // Januari
+  28, // Februari
+  31, // Maart
+  30, // April
+  31, // Mei
+  30, // Juni
+  31, // Juli
+  31, // Augustus
+  30, // September
+  31, // Oktober
+  30, // November
+  31, // December
+];
+
+export default AANTALDAGENPERMAAND;
