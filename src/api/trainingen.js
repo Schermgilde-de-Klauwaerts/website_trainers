3738fa75d898d1b8d9282d4d279d3312abfaed5c
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-11-02 15:26:36 +0100

    added .env file

--- src/api/trainingen.js
+++ src/api/trainingen.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `http://localhost:9000/api/trainingen`;
+const baseUrl = `${process.env.REACT_APP_API_URL}/transactions`;
 
 export const getAll = async () => {
   const { data } = await axios.get(baseUrl);
