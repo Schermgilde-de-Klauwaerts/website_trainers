e5ac8d34cbc3d164cef3777ad272dbc68c39e6a6
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-11-04 10:40:57 +0100

    wedstrijdenprovider in app.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import Documenten from "./pages/Documenten";
 import Login from "./pages/Login";
 
 import { TrainingenProvider } from "./contexts/TrainingenProvider";
+import { WedstrijdenProvider } from "./contexts/WedstrijdenProvider";
 
 function App() {
   return (
-    <TrainingenProvider>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="kalender" element={<Kalender />} />
-        <Route path="overzicht" element={<Overzicht />} />
-        <Route path="documenten" element={<Documenten />} />
-        <Route path="*" element={<Kalender />} />
-      </Routes>
-    </TrainingenProvider>
+    <WedstrijdenProvider>
+      <TrainingenProvider>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="kalender" element={<Kalender />} />
+          <Route path="overzicht" element={<Overzicht />} />
+          <Route path="documenten" element={<Documenten />} />
+          <Route path="*" element={<Kalender />} />
+        </Routes>
+      </TrainingenProvider>
+    </WedstrijdenProvider>
   );
 }
 
